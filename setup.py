7585b617f8f4c7a57b561f9319dd6219fd8e9b40
david-salac <info@davidsalac.eu>
2020-05-24 18:48:33 +0100

    Package structure modified

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/david-salac/Portable-spreadsheet-generator",
-    package_dir={'': 'src'},
+    packages=setuptools.find_packages(),
     install_requires=['numpy', 'XlsxWriter'],
     classifiers=[
         "Programming Language :: Python :: 3",
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-# Add the correct path to the sources
-sys.path.append(os.path.abspath(
-    os.path.join(os.path.dirname(__file__), '..', 'src/')
-))
